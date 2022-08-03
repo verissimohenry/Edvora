f0ec8b425b82ec47f97216b0ca397048bfb8924a
verissimohenry <verissimohenry04@gmail.com>
2022-08-03 18:09:53 +0100

    Modify app to next.js

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders app title element", () => {
-  render(<App />);
-  const titleElement = screen.getByText(/React Posts Sharer/i);
-  expect(titleElement).toBeInTheDocument();
-});
