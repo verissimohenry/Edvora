f592f516b78abdb1f20a19761cc93642bde796e3
verissimohenry <verissimohenry04@gmail.com>
2022-08-03 20:52:12 +0100

    Modidy navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import styled from 'styled-components';
+import Link from 'next/link';
+
+const Nav = styled.nav`
+  height: 80px;
+  background: #000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+`;
+
+const StyledLink = styled.a`
+  padding: 0rem 2rem;
+`;
 
 const Navbar = () => {
   return (
-    <div>
-        <h1>Navbar</h1>
-    </div>
-  )
-}
+    <Nav>
+      <div>
+        <Link href='/' passHref>
+          <StyledLink>NXT</StyledLink>
+        </Link>
+      </div>
+      <div>
+        <Link href='/' passHref>
+          <StyledLink>Home</StyledLink>
+        </Link>
+        <Link href='/about' passHref>
+          <StyledLink>About</StyledLink>
+        </Link>
+        <Link href='/contact' passHref>
+          <StyledLink>Contact</StyledLink>
+        </Link>
+      </div>
+    </Nav>
+  );
+};
 
 export default Navbar;
