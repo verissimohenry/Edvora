b50da17acdef72ff4bb338f396aa6d74145df648
verissimohenry <verissimohenry04@gmail.com>
2022-08-03 18:23:59 +0100

    Add new pages

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function About() {
   return (
     <div className={styles.container}>
       <Head>
